724bc63555193babe4f2c9c8dc844a719d0750bd
Ben Green <ben.green99@outlook.com>
2026-03-03 22:36:38 +0000

    Initial commit

---
+++ main.py
@@ -0,0 +1,31 @@
+import pygame
+from logger import log_state
+from constants import SCREEN_HEIGHT, SCREEN_WIDTH
+
+
+def main():
+    pygame.init()
+    screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
+    clock = pygame.time.Clock()
+    dt = 0
+
+    while True:
+        log_state()
+
+        for event in pygame.event.get():
+          if event.type == pygame.QUIT:
+            return
+
+        screen.fill("black")
+        pygame.display.flip()
+
+        dt = clock.tick(60) / 1000
+        print(dt)
+
+    print(f"Starting Asteroids with pygame version: {pygame.version.ver}")
+    print(f"Screen width: {SCREEN_WIDTH}")
+    print(f"Screen height: {SCREEN_HEIGHT}")
+
+
+if __name__ == "__main__":
+    main()
